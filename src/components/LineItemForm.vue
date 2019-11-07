28204bfdba07b2c043ccedd9109c2806488965c2
Mario Gruschetzki <mgruschetzki@gmail.com>
2019-11-07 10:23:56 +0100

    handle update line item by change input fields
    - commit new value to store
    - handle datepicker behavior

--- src/components/LineItemForm.vue
+++ src/components/LineItemForm.vue
@@ -4,28 +4,44 @@ v-container.elevation-12(fluid)
     v-list(dense)
       v-list-item
         v-list-item-content
-          v-date-picker(
-            v-if="openPicker"
-            label="Delivered At"
-            no-title
-            v-model="lineItem.deliveredAt"
-            :date-format="date => new Date(date).toDateString()"
-          )
-          v-text-field(v-model="lineItem.deliveredAt" @click="openPicker = !openPicker")
+          v-menu(
+            v-model="pickerOverlay"
+            :close-on-content-click="false"
+            transition="scale-transition"
+            offset-y
+            full-width
+            max-width="290px"
+            min-width="290px")
+
+            template(v-slot:activator="{ on }")
+              v-text-field(
+                readonly
+                v-on="on"
+                :value="date"
+                @change="updateLineItem('deliveredAt', $event)"
+              )
+
+            v-date-picker(
+              label="Delivered At"
+              prepend-icon="event"
+              no-title
+              v-model="date"
+              @input="pickerOverlay = false"
+            )
       v-list-item
         v-list-item-content
-          v-text-field(label="Price" v-model="lineItem.price")
+          v-text-field(label="Price" :value="lineItem.price" @input="updateLineItem('price', $event)")
         v-list-item-content
-          v-text-field(label="VAT Rate" v-model="lineItem.vatRate")
+          v-text-field(label="VAT Rate" :value="lineItem.vatRate" @input="updateLineItem('vatRate', $event)")
         v-list-item-content %
       v-list-item
         v-list-item-content
-          v-text-field(label="Quantity" v-model="lineItem.quantity")
+          v-text-field(label="Quantity" :value="lineItem.quantity" @input="updateLineItem('quatity', $event)")
         v-list-item-content
-          v-text-field(label="Unit" v-model="lineItem.unit")
+          v-text-field(label="Unit" :value="lineItem.unit" @input="updateLineItem('unit', $event)")
       v-list-item
         v-list-item-content.align-end
-          v-text-field(label="Description" v-model="lineItem.description")
+          v-text-field(label="Description" :value="lineItem.description" @input="updateLineItem('description', $event)")
 </template>
 
 <script>
@@ -33,8 +49,20 @@ v-container.elevation-12(fluid)
 export default {
   name: 'LineItemForm',
   props: {
-    lineItem: Object,
-    openPicker: Boolean,
+    lineItem: Object
   },
+  data () {
+    return {
+      openPicker: false,
+      date: this.lineItem.deliveredAt,
+      pickerOverlay: false
+    };
+  },
+  methods: {
+    updateLineItem(fieldKey, value) {
+      const nextLineItem = { [fieldKey]: value, id: this.lineItem.id };
+      this.$store.commit('updateLineItem', nextLineItem);
+    }
+  }
 };
 </script>
