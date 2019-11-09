e18a94066962c7cd38753c1ca4ca705e5eecf934
Mario Gruschetzki <mgruschetzki@gmail.com>
2019-11-09 13:22:17 +0100

    Use map-fields plugin and v-model

--- src/components/InvoiceForm.vue
+++ src/components/InvoiceForm.vue
@@ -20,7 +20,52 @@ v-container
           v-list(dense sub-group)
             v-subheader Line Items
             v-list-item(v-for='(item, i) in lineItems' :key='i')
-              LineItemForm(v-bind:lineItem="item")
+              // LineItemForm(v-bind:lineItem="item")
+
+              v-container.elevation-12(fluid)
+                p {{ item.description }}
+                v-form
+                  v-list(dense)
+                    v-list-item
+                      v-list-item-content
+                        v-menu(
+                          v-model="item.pickerOverlay"
+                          close-on-content-click
+                          transition="scale-transition"
+                          offset-y
+                          max-width="290px"
+                          min-width="290px")
+
+                          template(v-slot:activator="{ on }")
+                            v-text-field(
+                              readonly
+                              v-on="on"
+                              :value="item.deliveredAt"
+
+                            )
+
+                          v-date-picker(
+                            label="Delivered At"
+                            prepend-icon="event"
+                            no-title
+                            v-model="item.deliveredAt"
+                          )
+                    v-list-item
+                      v-list-item-content
+                        v-text-field(label="Price" v-model="item.price")
+                      v-list-item-content
+                        v-text-field(label="VAT Rate" v-model="item.vatRate")
+                      v-list-item-content %
+                    v-list-item
+                      v-list-item-content
+                        v-text-field(label="Quantity" v-model="item.quantity")
+                      v-list-item-content
+                        v-text-field(label="Unit" v-model="item.unit")
+                    v-list-item
+                      v-list-item-content.align-end
+                        v-text-field(label="Description" v-model="item.description")
+
+
   v-fab-transition
     v-btn(
       color="pink"
